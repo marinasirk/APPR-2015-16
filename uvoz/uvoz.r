101e5c27a056bfa542687b98e299c5efe2c93292
Marina Sirk <mariiina.ms@gmail.com>
2015-12-15 20:54:09 +0100

    html tabela

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -2,8 +2,11 @@
 
 library(dplyr)
 library(gsubfn)
-require(ggplot2)
+library(ggplot2)
 library(rvest)
+library(XML)
+
+
 
 # TABELA CSV  
 
@@ -94,7 +97,7 @@ Encoding(razveze2[[1]]) <- "UTF-8"
 razveze2 <- t(apply(razveze2, 1, function(x) c(rep(NA, sum(is.na(x))), x[!is.na(x)])))
 razveze2 <- razveze2[-nrow(razveze2),]
 
-# names(razveze2) <- c("REGIJA", "ST.OTROK", "LETO", "STEVILO.RAZVEZ")
+colnames(razveze2) <- c("REGIJA", "ST.OTROK", "LETO", "STEVILO.RAZVEZ")
 
 
 razveze2[,4] <- as.numeric(razveze2[,4])
@@ -102,17 +105,10 @@ razveze2[,4] <- as.numeric(razveze2[,4])
 razveze2 <- uredi(razveze2, 1, 1, 34)
 razveze2 <- uredi(razveze2, 1, 2, 6)
 
-# st.otrok <- c(filter(razveze2, REGIJA == "Pomurska")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Podravska")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Koroška")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Savinjska")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Zasavska")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Spodnjeposavska")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Jugovzhodna Slovenija")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Osrednjeslovenska")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Gorenjska")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Notranjsko-kraška")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Goriška")$STEVILO.RAZVEZ %>% sum(),
-#               filter(razveze2, REGIJA == "Obalno-kraška")$STEVILO.RAZVEZ %>% sum() )
+st.otrok <- c(filter(razveze2, ST.OTROK == "Brez otrok")$STEVILO.RAZVEZ %>% sum(),
+              filter(razveze2, ST.OTROK == "Eden")$STEVILO.RAZVEZ %>% sum(),
+              filter(razveze2, ST.OTROK == "Dva")$STEVILO.RAZVEZ %>% sum(),
+              filter(razveze2, ST.OTROK == "Trije")$STEVILO.RAZVEZ %>% sum(),
+              filter(razveze2, ST.OTROK == "Štirje ali več")$STEVILO.RAZVEZ %>% sum() )
 
-#otroci <- data.frame(regije, st.otrok)
+otroci <- data.frame(regije, st.otrok)
