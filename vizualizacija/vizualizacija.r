fc9c08994326cb074f80b21c0343c2d50f0ced3d
Marina Sirk <mariiina.ms@gmail.com>
2016-01-06 22:02:33 +0100

    zemljevid za vsote

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -8,6 +8,10 @@ library(dplyr)
 zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
                              "SVN_adm1", encoding = "UTF-8")
 
+rownames(vsota) <- vsota$REGIJE
+zemljevid1 <- preuredi(vsota, zemljevid, "NAME_1")
+
+
 pretvori.zemljevid <- function(zemljevid) {
   fo <- fortify(zemljevid)
   data <- zemljevid@data
@@ -15,5 +19,14 @@ pretvori.zemljevid <- function(zemljevid) {
   return(inner_join(fo, data, by="id"))
 }
 
+# Regije na zemljevidu
 
+#zemljevid$razveze <- zemljevid1$ODSTOTKI
+#zemljevid2 <- pretvori.zemljevid(zemljevid1)
 
+zem.reg2014 <- ggplot() + geom_polygon(data = zemljevid1, aes(x=long, y=lat, group=group,
+                                                           fill=ODSTOTKI),
+                                       color = "grey") +
+  scale_fill_gradient(low="#3F7F3F", high="#00FF00") +
+  guides(fill = guide_colorbar(title = "Površina"))
+print(zem.reg2014)
