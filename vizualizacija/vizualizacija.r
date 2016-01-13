7dedea266e00383ca83325e74c680df2d85d5f77
Marina Sirk <mariiina.ms@gmail.com>
2016-01-13 11:37:52 +0100

    zemljevid

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -29,7 +29,7 @@ zemljevid2 <- pretvori.zemljevid(zemljevid)
 vsota.zemljevid <- ggplot() + geom_polygon(data = zemljevid2, aes(x=long, y=lat, group=group,
                                                          fill=razveze.odstotki),color = "grey30") +
   scale_fill_gradient(low="cyan", high="cyan4") +
-  guides(fill = guide_colorbar(title = "Porazdelitev števila\nrazvez po regijah"))
+  guides(fill = guide_colorbar(title = "Porazdelitev\nštevila razvez\npo regijah"))
 
 #print(vsota.zemljevid)
 
