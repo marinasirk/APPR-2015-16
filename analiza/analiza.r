b4de401f566b54534c340de2ff2fcacad1a1935b
Marina Sirk <mariiina.ms@gmail.com>
2016-01-13 12:02:05 +0100

    analiza

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,2 +1,9 @@
 # 4. faza: Analiza podatkov
 
+g <- ggplot(razveze3, aes(y=STEVILO.RAZVEZ, x=LETO)) + geom_point()
+
+# metoda najmanjših kvadratov
+
+g <- g + geom_smooth(method = "lm", formula = y ~ x + I(x^2))
+
+print(g)
