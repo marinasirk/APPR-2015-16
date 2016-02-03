d71e67c899ccacdf3e4117a16f28f383d91861c6
Marina Sirk <mariiina.ms@gmail.com>
2016-02-03 19:00:21 +0100

    metoda najmanjših kvadaratov

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -4,6 +4,8 @@ g <- ggplot(razveze3, aes(y=STEVILO.RAZVEZ, x=LETO)) + geom_point()
 
 # metoda najmanjših kvadratov
 
-g <- g + geom_smooth(method = "lm", formula = y ~ x + I(x^2))
+g <- g + geom_smooth(method = "lm", formula = y ~ x)
 
 print(g)
+
+# manjka še razvrščanje v skupine
